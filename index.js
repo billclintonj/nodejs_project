659f36a2f458bf4e881e708a420ac77379994dab
billclintonj <billclinton_j@outlook.com>
2022-06-25 09:33:46 +0530

    Updated listen command with process func

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 8080;
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root:__dirname});
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Now listening on port ${port}`);
 })
